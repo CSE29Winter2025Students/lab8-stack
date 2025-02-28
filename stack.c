798c5f9ef09bb6f7ab695f6c1048222aa14066fc
Peng <h4peng@ieng6-202.ucsd.edu>
2025-02-27 17:27:58 -0800

    Remove push() definition

--- stack.c
+++ stack.c
@@ -11,10 +11,3 @@ void clear() {
 }
 
 // Implement your team's function here!
-
-void push(int i) {
-    struct Node *new = malloc(sizeof(struct Node));
-    new->data = i;
-    new->next = stackTop;
-    stackTop = new;
-}
